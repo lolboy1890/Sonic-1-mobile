eb70a2ac4f2e46ce06ab9fe5b8a7d38150826bbb
lolboy1890 <lloydofficial57@gmail.com>
2025-03-29 12:29:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10560,14 +10560,6 @@ if (Module['noInitialRun']) shouldRunNow = false;
 
 run();
 
-        function save(state) {
-          localStorage.setItem('gameData', JSON.stringify(state));
-        }
-
-        function load() {
-          const data = localStorage.getItem('gameData');
-          return data ? JSON.parse(data) : null;
-        }
-
+    
 
 
